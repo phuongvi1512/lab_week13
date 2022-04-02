adef44827ebe764719416a12db38554ed95526fe
Phuong Vi Dang <phuongvi1512@gmail.com>
2022-04-01 23:25:21 -0700

    adding css style: align text to center, change background color

--- src/app.js
+++ src/app.js
@@ -2,7 +2,19 @@
 const originalContainer = document.querySelector('.original-container');
 
 originalContainer.addEventListener('click', (evt) => {
-    const copiedText = `<div>${evt.target.outerHTML}</div> `;
+    const copiedTextDiv = `<div>${evt.target.outerHTML}</div> `;
     const copyContainer = document.querySelector('.copy-container');
-    copyContainer.insertAdjacentHTML('afterbegin',copiedText);
+    copyContainer.insertAdjacentHTML('afterbegin',copiedTextDiv);
 });
+
+
+const clearButton = document.querySelector('button')
+
+clearButton.addEventListener('click', () => {
+    const childDivcopyContainer = document.querySelector('.copy-container div')
+    function removeDiv(div) {
+        div.remove()
+    }
+    removeDiv(childDivcopyContainer)
+});
+
